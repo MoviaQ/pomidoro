659d65050c95e36007fb01ede3d131f3d0965a3f
Maciej Klimczyk <maciejklimczyk@MacBook-Pro-Maciej.local>
2024-09-15 22:49:29 +0200

    Fixed some bugs

--- pomidoro/config/firebase.js
+++ pomidoro/config/firebase.js
@@ -1,16 +1,15 @@
-// Import the functions you need from the SDKs you need
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDBoLJHOIlQMwTByvA2cLF0YqG1W1Y4NUE",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: "pomidoro-9a244.firebaseapp.com",
   projectId: "pomidoro-9a244",
   storageBucket: "pomidoro-9a244.appspot.com",
   messagingSenderId: "992279755197",
-  appId: "1:992279755197:web:b8755630098342b0ff7ad4",
-  measurementId: "G-MLH26N15XJ",
+  appId: "1:992279755197:web:9212262498aa2fc6ff7ad4",
+  measurementId: "G-L3EFPDPTP9",
 };
 
 // Initialize Firebase
